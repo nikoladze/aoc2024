2604a7b0a1b6ae9d0c3fffd018b4e825d4d7eb7d
Nikolai Krug <nikoladze@posteo.de>
2024-12-15 14:31:32 +0100

    refactor solution for day12

--- day12/solver.py
+++ day12/solver.py
@@ -11,35 +11,37 @@ def parse(raw_data):
     return raw_data.strip().splitlines()
 
 
-@watch.measure_time
-def solve1(data):
+def find_clusters(data):
     pos_cluster = {}
     clusters = []
     for i, row in enumerate(data):
         for j, x in enumerate(row):
-            #print(i, j, x)
             for di, dj in [(-1, 0), (0, -1)]:
                 ii, jj = i + di, j + dj
                 if ii < 0 or jj < 0 or i >= len(data) or j >= len(data[0]):
                     continue
-                if x != data[ii][jj]:
+                elif x != data[ii][jj]:
                     continue
-                if (i, j) not in pos_cluster:
+                elif (i, j) not in pos_cluster:
+                    # add to neighboring cluster
                     pos_cluster[ii, jj].add((i, j))
                     pos_cluster[i, j] = pos_cluster[ii, jj]
-                if pos_cluster[i, j] is not pos_cluster[ii, jj]:
-                    # merge
+                elif pos_cluster[i, j] is not pos_cluster[ii, jj]:
+                    # merge with neighboring cluster
                     pos_cluster[ii, jj].update(pos_cluster[i, j])
                     clusters.remove(pos_cluster[i, j])
                     for pos in pos_cluster[i, j]:
                         pos_cluster[pos] = pos_cluster[ii, jj]
-
             if (i, j) not in pos_cluster:
                 cluster = set([(i, j)])
                 pos_cluster[i, j] = cluster
                 clusters.append(cluster)
-                #print("new cluster")
-            #input()
+    return clusters
+
+
+@watch.measure_time
+def solve1(data):
+    clusters = find_clusters(data)
     total = 0
     for cluster in clusters:
         area = len(cluster)
@@ -49,80 +51,39 @@ def solve1(data):
                 if (i + di, j + dj) not in cluster:
                     perimeter += 1
         total += area * perimeter
-        #print(f"{area} * {perimeter} = {area * perimeter}")
     return total
+
 
 @watch.measure_time
 def solve2(data):
-    pos_cluster = {}
-    clusters = []
-    for i, row in enumerate(data):
-        for j, x in enumerate(row):
-            #print(i, j, x)
-            for di, dj in [(-1, 0), (0, -1)]:
-                ii, jj = i + di, j + dj
-                if ii < 0 or jj < 0 or i >= len(data) or j >= len(data[0]):
-                    continue
-                if x != data[ii][jj]:
-                    continue
-                if (i, j) not in pos_cluster:
-                    pos_cluster[ii, jj].add((i, j))
-                    pos_cluster[i, j] = pos_cluster[ii, jj]
-                if pos_cluster[i, j] is not pos_cluster[ii, jj]:
-                    # merge
-                    pos_cluster[ii, jj].update(pos_cluster[i, j])
-                    clusters.remove(pos_cluster[i, j])
-                    for pos in pos_cluster[i, j]:
-                        pos_cluster[pos] = pos_cluster[ii, jj]
-
-            if (i, j) not in pos_cluster:
-                cluster = set([(i, j)])
-                pos_cluster[i, j] = cluster
-                clusters.append(cluster)
-                #print("new cluster")
-            #input()
+    clusters = find_clusters(data)
     total = 0
     for cluster in clusters:
-        debug(cluster)
         area = len(cluster)
-        border = set()
-        border_i = {}
-        border_j = {}
+        borders = {}
         for i, j in cluster:
             for di, dj in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
                 if (i + di, j + dj) not in cluster:
-                    # track borders in all x coords, y coords
-                    border.add((i, j))
+                    # group borders by direction and by common
+                    # coordinates in i (horizontal borders)
+                    # and j (vertical borders) coordinates
                     if dj != 0:
-                        border_i.setdefault(dj, {}).setdefault(j, set()).add(i)
-                    if di != 0:
-                        border_j.setdefault(di, {}).setdefault(i, set()).add(j)
-        print(border_i)
-        print(border_j)
+                        common = j
+                        varying = i
+                    elif di != 0:
+                        common = i
+                        varying = j
+                    borders.setdefault((di, dj, common), set()).add(varying)
         sides = 0
-        for border in [border_i, border_j]:
-            for values_d in border.values():
-                for values in values_d.values():
-                    prev = None
-                    for x in sorted(values):
-                        if prev is None or abs(x - prev) != 1:
-                            sides += 1
-                        prev = x
-        print(sides)
+        for values in borders.values():
+            # find groups of connected border points
+            prev = None
+            for x in sorted(values):
+                if prev is None or abs(x - prev) != 1:
+                    sides += 1
+                prev = x
         total += sides * area
     return total
-
-def debug(points):
-    maxi = max(i for i, j in points)
-    maxj = max(j for i, j in points)
-    mini = min(i for i, j in points)
-    minj = min(j for i, j in points)
-    grid = [["." for j in range(maxj - minj + 1)] for i in range(maxi - mini + 1)]
-    for i, j in points:
-        grid[i - mini][j - minj] = "X"
-    print()
-    print("\n".join("".join(row) for row in grid))
-    print()
 
 
 if __name__ == "__main__":
